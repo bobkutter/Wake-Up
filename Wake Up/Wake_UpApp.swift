d7720b4d1f92a03688ecb64d322f2b33f90ca54a
Bob Kutter <bobkutter@gmail.com>
2021-10-22 16:32:45 -0700

    Initial Commit

---
+++ Wake Up/Wake_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wake_UpApp.swift
+//  Wake Up
+//
+//  Created by Robert Kutter on 10/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Wake_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
